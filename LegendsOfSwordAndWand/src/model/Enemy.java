4284022c274bbdc984b6d04426f2372d19a40e9a
ahmede06 <ahmede06@my.yorku.ca>
2026-03-25 23:33:19 -0400

    Delete LegendsOfSwordAndWand/src directory

--- LegendsOfSwordAndWand/src/model/Enemy.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class Enemy extends Unit {
-    public Enemy(String name, int level, int hp, int attack, int defense) {
-        super(name, level, hp, attack, defense);
-    }
-}
--- LegendsOfSwordAndWand/src/model/Hero.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class Hero extends Unit {
-    public Hero(String name, int level, int hp, int attack, int defense) {
-        super(name, level, hp, attack, defense);
-    }
-}
